1403364ba3188991987ff3e2c7b1123c3e5ecdde
MR.shipet <superiorshipet@gmail.com>
2025-11-12 17:05:40 +0200

    f

--- podcasty/obj/Debug/net8.0/podcasty.AssemblyInfo.cs
+++ podcasty/obj/Debug/net8.0/podcasty.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("podcasty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8b393836547856bfae7029e94da47647e66b26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d58327375f53ff4384755f2aead15b867bed4a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("podcasty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("podcasty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
