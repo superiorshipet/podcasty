2f32bd05738aad15da7610a1cabc649e83634fcd
MR.shipet <superiorshipet@gmail.com>
2025-11-12 13:59:38 +0200

    made the git ignore update

--- podcasty/obj/Debug/net8.0/podcasty.AssemblyInfo.cs
+++ podcasty/obj/Debug/net8.0/podcasty.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("podcasty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9adfa06c9bae5ca69a1d986579716e1926d2c8cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7fbc88d5cab0b7f299cd97c375342063e46b29e")]
 [assembly: System.Reflection.AssemblyProductAttribute("podcasty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("podcasty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
