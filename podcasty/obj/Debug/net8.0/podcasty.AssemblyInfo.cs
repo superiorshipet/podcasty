6b583fad8e0ebdfe286b96376d671646fb72d8a2
MR.shipet <superiorshipet@gmail.com>
2025-11-12 16:20:01 +0200

    done i hope

--- podcasty/obj/Debug/net8.0/podcasty.AssemblyInfo.cs
+++ podcasty/obj/Debug/net8.0/podcasty.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("podcasty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb24118ba4a85f1b929e6fedcafb792381bd345")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a67d7e6d3ce0f4814a49a13fce5b3efad28516bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("podcasty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("podcasty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
