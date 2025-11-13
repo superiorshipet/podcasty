851d966119e1e1f3f69d13e62fd4eaec0a9230bb
Kerolos Osama <Kero@gmail.com>
2025-11-13 22:46:14 +0200

    filter-sort

---
+++ podcasty/Interfaces/ISortingRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using podcasty.Models;
+
+namespace podcasty.Interfaces
+{
+    public interface ISortingRepository
+    {
+        Task<List<Episode>> SortByTimeDescending();
+        Task<List<Episode>> SortByTimeAscending();
+        Task<List<Episode>> SortByDurationDescending();
+        Task<List<Episode>> SortByDurationAscending();
+        Task<List<Episode>> SortByViewsAscending();
+        Task<List<Episode>> SortByViewsDescending();
+
+    }
+}
